abbc5b6d991115aa404af07ef178dd30f7cae3d9
ytnuf <161308826+ytnuf@users.noreply.github.com>
2024-12-29 16:47:35 +0000

    Use generate expressions for compiler flags
    Generator expressions are rather annoying due to undebuggability
    and really horrible syntax
    
    But it is considred to be the "proper" way of doing things,
    hence this commit

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -2,12 +2,8 @@
 option(ENABLE_ADDITIONAL_WARNINGS "Turn on addtional warnings" OFF)
 
 # The sanitisers runtime libraries (e.g libubsan) may need to be installed
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    option(ENABLE_ADDRESS_SANTISER "Turn on sanitiser for memory errors, can noticeable slow down execution" OFF)
-endif()
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    option(ENABLE_UNDEFINED_BEHAVIOUR_SANTISER "Turn on sanitiser for undefined behaviour" OFF)
-endif()
+option(ENABLE_ADDRESS_SANTISER "Turn on sanitiser for memory errors, can noticeable slow down execution (for GCC, Clang & MSVC)" OFF)
+option(ENABLE_UNDEFINED_BEHAVIOUR_SANTISER "Turn on sanitiser for undefined behaviour (for GCC & Clang)" OFF)
 
 string(TOLOWER "${CMAKE_SYSTEM_NAME}.${CMAKE_SYSTEM_PROCESSOR}" _GDEXTENSION_TARGET_PLATFORM_)
 set(GDEXTENSION_TARGET_PLATFORM "${_GDEXTENSION_TARGET_PLATFORM_}" CACHE STRING "The platform this gdextension is targetted for")
@@ -17,67 +13,97 @@ add_library("project-options" INTERFACE)
 add_library("project::options" ALIAS "project-options")
 
 
-if(ENABLE_ADDITIONAL_WARNINGS)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        #GCC and Clang share these warnings
-        target_compile_options("project-options" INTERFACE
-            "-Wall"
-            "-Wextra"
-            "-Wpedantic"
-            "-Warray-bounds"
-            "-Wcast-align"
-            "-Wconversion"
-            "-Wdangling-else"
-            "-Wimplicit-fallthrough"
-            "-Wfloat-equal"
-            "-Winit-self"
-            "-Wmain"
-            "-Wmissing-declarations"
-            "-Wnon-virtual-dtor"
-            "-Wparentheses"
-            "-Wpointer-arith"
-            "-Wredundant-decls"
-            "-Wswitch"
-            "-Wstrict-overflow"
-            "-Wuninitialized"
-            "-Wundef"
-            "-Wunreachable-code"
-            "-Wunused"
+# Some helper variables to help with generator expression since they are notorious unreadable
+set(COMPILER_IS_GCC "$<CXX_COMPILER_ID:GNU>")
+set(COMPILER_IS_CLANG "$<OR:$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:Clang>>")
+set(COMPILER_IS_GCC_OR_CLANG "$<OR:${COMPILER_IS_GCC},${COMPILER_IS_CLANG}>")
+set(COMPILER_IS_MSVC "$<CXX_COMPILER_ID:MSVC>")
 
-            "-Wno-float-conversion"
-        )
-        #Clang has additional warnings
-        if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-            target_compile_options("project-options" INTERFACE
-                "-Wcovered-switch-default"
-                "-Wdeprecated"
-                "-Wdeprecated-copy"
-                "-Wdeprecated-copy-dtor"
-                "-Wmissing-prototypes"
-                "-Wmissing-variable-declarations"
-                "-Wsuggest-destructor-override"
-                "-Wnewline-eof"
-                "-Wunused-exception-parameter"
-            )
-        endif()
-    else()
-        message(AUTHOR_WARNING "TODO: Add MSVC warnings")
+
+if(ENABLE_ADDITIONAL_WARNINGS)
+    target_compile_options("project-options" INTERFACE
+        $<${COMPILER_IS_GCC_OR_CLANG}:
+            # Basic warnings
+            -Wall
+            -Wextra
+            -Wpedantic
+            # Additional warnings
+            -Warray-bounds
+            -Wcast-align
+            -Wconversion
+            -Wdangling-else
+            -Wimplicit-fallthrough
+            -Wfloat-equal
+            -Winit-self
+            -Wmain
+            -Wmissing-declarations
+            -Wnon-virtual-dtor
+            -Woverloaded-virtual
+            -Wparentheses
+            -Wpointer-arith
+            -Wredundant-decls
+            -Wshadow
+            -Wswitch
+            -Wstrict-overflow
+            -Wuninitialized
+            -Wundef
+            -Wunreachable-code
+            -Wunused
+            -Wwrite-strings
+            # Unfortunately gdextension code typically has a lot of float & double mixing
+            -Wno-float-conversion
+            -Wno-implicit-float-conversion
+        >
+        $<${COMPILER_IS_GCC}:
+            -Walloc-zero
+            -Wduplicated-branches
+            -Wduplicated-cond
+            -Wlogical-op
+        >
+        $<${COMPILER_IS_CLANG}:
+            -Wcovered-switch-default
+            -Wdeprecated
+            -Wdeprecated-copy
+            -Wdeprecated-copy-dtor
+            -Wdocumentation
+            -Wmissing-prototypes
+            -Wmissing-variable-declarations
+            -Wsuggest-destructor-override
+            -Wnewline-eof
+            -Wunused-exception-parameter
+        >
+    )
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        message(AUTHOR_WARNING "TODO: Add MSVC warning flags")
     endif()
 endif()
 
 
 if(ENABLE_UNDEFINED_BEHAVIOUR_SANTISER)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        target_compile_options("project-options" INTERFACE "-fsanitize=undefined")
-        target_link_options("project-options" INTERFACE "-fsanitize=undefined")
-    endif()
+    target_compile_options("project-options" INTERFACE
+        $<${COMPILER_IS_GCC_OR_CLANG}:
+            -fsanitize=undefined
+        >
+    )
+    target_link_options("project-options" INTERFACE
+        $<${COMPILER_IS_GCC_OR_CLANG}:
+            -fsanitize=undefined
+        >
+    )
 endif()
 
 if(ENABLE_ADDRESS_SANTISER)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        target_compile_options("project-options" INTERFACE "-fsanitize=address")
-        target_link_options("project-options" INTERFACE "-fsanitize=address")
-    else()
+    target_compile_options("project-options" INTERFACE
+        $<${COMPILER_IS_GCC_OR_CLANG}:
+            -fsanitize=address
+        >
+    )
+    target_link_options("project-options" INTERFACE
+        $<${COMPILER_IS_GCC_OR_CLANG}:
+            -fsanitize=address
+        >
+    )
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         message(AUTHOR_WARNING "TODO: Enable MSVC flags for ASAN")
     endif()
 endif()
